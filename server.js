611e633f06b43a3645b74806ef25f8fa1abfbe22
Alexa Scott <arscott4792@gmail.com>
2018-03-14 11:24:03 -0700

    updated public

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 app.use(express.json());
 
 // Mount router on "/v2"
-app.use('/v2', notesRouterV2);
+app.use('/api', notesRouterV2);
 
 // Catch-all 404
 app.use(function (req, res, next) {
