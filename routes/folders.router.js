9385b67f6500d588b0adef0b08f6f205bbe9e5bd
Alexa Scott <arscott4792@gmail.com>
2018-03-14 13:23:14 -0700

    created route file and routes

--- routes/folders.router.js
+++ routes/folders.router.js
@@ -1,6 +1,86 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+
 const knex = require('../knex');
+
+router.get('/folders', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('folders')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+router.get('/folders/:id', (req, res, next) => {
+  knex.select('id', 'name')
+    .where('id', req.params.id)
+    .from('folders')
+    .then(([result]) => {
+      if(result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(next);
+});
+
+router.post('/folders', (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = { name };
+
+  knex.insert(newItem)
+    .into('folders')
+    .returning(['id', 'name'])
+    .then(([result]) => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
+router.put('/folders/:id', (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateItem = { name };
+
+  knex('folders')
+    .update(updateItem)
+    .where('id', req.params.id)
+    .returning(['id', 'name'])
+    .then(([result]) => {
+      if(result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
+});
+
+router.delete('/folders/:id', (req, res, next) => {
+  knex.del()
+    .where('id', req.params.id)
+    .from('folders')
+    .then(count => {
+      res.status(204).end();
+    })
+    .catch(next);
+});
+
+module.exports = router;
