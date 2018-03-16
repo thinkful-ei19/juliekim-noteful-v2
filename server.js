85b2f8e80b38fbee841793c94edcc31dde77cccc
Alexa Scott <arscott4792@gmail.com>
2018-03-16 08:56:45 -0700

    installed npm for testing and updated server.js

--- server.js
+++ server.js
@@ -47,8 +47,13 @@ app.use(function (err, req, res, next) {
 });
 
 // Listen for incoming connections
+if(require.main === module) {
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
 });
+}
+
+//Export for testing
+module.exports = app;
